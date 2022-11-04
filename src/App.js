07d442eb96c8a0e77b84f38858838c4608df2955
rohan09-raj <rajrohan1914@gmail.com>
2022-11-04 23:31:06 +0530

    Added MUI and built some components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,113 @@
+import React, {useState} from 'react';
+import GoogleIcon from '@mui/icons-material/Google';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import {Button, Modal, Box, Typography} from '@mui/material';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/bootstrap.css';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  borderRadius: '6px',
+  boxShadow: 24,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'justify',
+  p: 4,
+};
+
 export default function App() {
+  const [open, setOpen] = useState(false);
+  const [openTwo, setOpenTwo] = useState(false);
+  const handleClick = () => setOpen(!open);
+  const [number, setNumber] = useState('');
+
+  const handleClickTwo = () => {
+    setOpen(false);
+    setOpenTwo(!openTwo);
+  };
+
+  console.log(number);
+
   return (
     <div>
-      File structure done! 
+      <Button onClick={handleClick} variant='contained'>
+        Login
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClick}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box sx={style}>
+          <Typography
+            id='modal-modal-title'
+            variant='h6'
+            component='h2'
+            align='center'
+          >
+            GET STARTED
+          </Typography>
+          <Button
+            onClick={handleClickTwo}
+            variant='contained'
+            style={{margin: '10px'}}
+          >
+            Sign in with Mobile Number <PhoneIphoneIcon />
+          </Button>
+          <Button
+            onClick={handleClickTwo}
+            variant='contained'
+            style={{margin: '10px'}}
+          >
+            Sign in with Google <GoogleIcon />
+          </Button>
+        </Box>
+      </Modal>
+
+      <Modal
+        open={openTwo}
+        onClose={handleClickTwo}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box sx={style}>
+          <Typography
+            id='modal-modal-title'
+            variant='h6'
+            component='h2'
+            align='center'
+            style={{marginBottom: '10px'}}
+          >
+            Enter your Mobile Number
+          </Typography>
+          <PhoneInput
+            country='in'
+            value={number}
+            onChange={(value) => {
+              setNumber(value);
+            }}
+            enableSearch={true}
+            countryCodeEditable={false}
+            inputStyle={{width: '100%'}}
+            searchStyle={{width: '95%', padding: '10px'}}
+            enableClickOutside={false}
+            searchPlaceholder='Search for your Country'
+          />
+          <Button
+            onClick={handleClickTwo}
+            variant='contained'
+            style={{marginTop: '10px'}}
+          >
+            Continue
+          </Button>
+        </Box>
+      </Modal>
     </div>
   );
-};
+}
